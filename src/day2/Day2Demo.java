f2c65e45ecb517876e6775a7da1042da63606e0b
malzu <malzu@10.0.0.198>
2023-04-07 20:52:14 -0400

    Hola

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -2,8 +2,11 @@ package day2;
 
 public class Day2Demo {
 	
-	public void getInfo() {
-		System.out.println("Pushing code...");
-		
+	public void add(int a, int b) {
+		System.out.println(a+b);
+	}
+	
+	public void divid() {
+		System.out.println(8/2);
 	}
 }
